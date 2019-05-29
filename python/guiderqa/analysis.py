1e7f7aa21a4ff4b41bf813e443be41747acb479d
José Sánchez-Gallego <gallegoj@uw.edu>
2019-05-29 09:50:39 -0600

    Add some analysis tools

--- python/guiderqa/analysis.py
+++ python/guiderqa/analysis.py
@@ -7,10 +7,13 @@
 # @Filename: analysis.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 # @Copyright: José Sánchez-Gallego
+import warnings
 
 import astropy
 import numpy
 import pandas
+
+from .core.exceptions import GuiderQAUserWarning
 
 
 def groupby_range(dataframe, range_column, value_column, bins=5, index_column='mjd',
@@ -31,6 +34,12 @@ def groupby_range(dataframe, range_column, value_column, bins=5, index_column='m
         The index column to do the initial group by.
     aggfunc : function, optional
         A function to be passed to `pandas.pivot_table` to aggregate the data.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame indexed by MJD in which the columns are the altitude ranges
+        and the values are the aggregated values for ``value_column``.
 
     """
 
@@ -65,6 +74,12 @@ def get_ha(dataframe, ra_column='ra', obstime_column='date_obs', observatory='ap
     observatory : str, optional
         Either ``'apo'`` or ``'lco'``.
 
+    Returns
+    -------
+    pandas.Series
+        A Series with the HA values corresponding to the RA and observing
+        times.
+
     """
 
     obs = astropy.coordinates.EarthLocation.of_site(observatory.lower())
@@ -75,3 +90,58 @@ def get_ha(dataframe, ra_column='ra', obstime_column='date_obs', observatory='ap
     ha = local_sidereal.deg - ra
 
     return ha
+
+
+def get_dalt_daz(dataframe, obstime_column='date_obs', observatory='apo'):
+    """Returns a `~pandas.DataFrame` with the ``dalt, daz`` values.
+
+    Assumes that the columns ``dra, ddec, alt, az, ra, dec`` are present in the
+    input ``dataframe``.
+
+    Parameters
+    ----------
+    dataframe : pandas.DataFrame
+        The DataFrame containing the data.
+    obstime_column : str, optional
+        The name of the column holding the observed time values. The values
+        must be in the form of an ISO timestamp.
+    observatory : str, optional
+        Either ``'apo'`` or ``'lco'``.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame with the same indexing as the input ``dataframe`` with two
+        columns, ``dalt``, and ``daz``.
+
+    """
+
+    dataframe = dataframe.copy()
+
+    obs = astropy.coordinates.EarthLocation.of_site(observatory.lower())
+
+    # Forces time to pandas datetime to remove badly formated times.
+    dataframe[obstime_column] = pandas.to_datetime(dataframe[obstime_column], 'coerce')
+
+    if sum(dataframe[obstime_column].isna()) > 0:
+        warnings.warn('removing {} rows because the obstime is badly formatted'
+                      .format(sum(dataframe[obstime_column].isna())), GuiderQAUserWarning)
+
+    dataframe.dropna(subset=[obstime_column, 'dra', 'ddec', 'ra', 'dec', 'alt', 'az'],
+                     inplace=True)
+
+    times = astropy.time.Time(list(dataframe[obstime_column].astype(str).values),
+                              location=obs, format='iso')
+
+    ra_dec = astropy.coordinates.SkyCoord(ra=(dataframe.ra + dataframe.dra).values,
+                                          dec=(dataframe.dec + dataframe.ddec).values,
+                                          unit='deg')
+    alt_az = ra_dec.transform_to(astropy.coordinates.AltAz(obstime=times, location=obs))
+
+    dalt = alt_az.alt.deg - dataframe.alt
+    daz = alt_az.az.deg - dataframe.az
+
+    return_df = pandas.concat([dalt, daz], axis=1)
+    return_df.columns = ['dalt', 'daz']
+
+    return return_df
